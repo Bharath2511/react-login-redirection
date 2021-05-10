08a1e1523df96afaf49a5134f74dff87e0e83235
Bharath2511 <bharathchandra630@gmail.com>
2021-05-10 07:51:39 +0000

    Initial Commit

---
+++ src/components/Products/index.js
@@ -0,0 +1,27 @@
+// import {withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+
+import Header from '../Header'
+import './index.css'
+
+const Products = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <>
+      <Header />
+      <div className="products-section">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+          className="product-image"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Products
